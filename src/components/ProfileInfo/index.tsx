c7ac9f531f114660915ff7c2516fa97971191c8a
Gabriel Henrique <contato@gabrielhenrique.net>
2021-06-03 20:04:03 -0300

    update profile component

--- src/components/ProfileInfo/index.tsx
+++ src/components/ProfileInfo/index.tsx
@@ -1,3 +1,4 @@
+import { Twitter } from '@styled-icons/boxicons-logos'
 import { Link } from '../Footer/styles'
 import * as S from './styles'
 
@@ -13,6 +14,7 @@ type ProfileInfoProps = {
         location?: string;
         email?: string;
         blog?: string;
+        twitter_username?: string;
     }
 }
 
@@ -22,13 +24,61 @@ const ProfileInfo: React.FC<ProfileInfoProps> = ({ user }) => {
             <S.AvatarBox>
                 <S.Avatar src={user.avatar_url} alt={user.login} />
                 <S.AvatarInfo>
-                    <strong>{user.name}</strong>
-                    <span>{user.login}</span>
-                    <p>{user.bio}</p>
+                    <h1>
+                        <S.AvatarFullname>{user.name}</S.AvatarFullname>
+                        <S.AvatarNickname>{user.login}</S.AvatarNickname>
+                    </h1>
+                    <S.AvatarDescription>
+                        <S.AvatarBio>{user.bio}</S.AvatarBio>
+                        <S.FollowButton>Follow</S.FollowButton>
+                        <S.AvatarValues>
+                            <S.AvatarValue>
+                                <strong>{user.followers}</strong> followers
+                            </S.AvatarValue>
+                            <S.AvatarValue>
+                                <strong>{user.following}</strong> following
+                            </S.AvatarValue>
+                        </S.AvatarValues>
+                    </S.AvatarDescription>
                 </S.AvatarInfo>
+                {/* <S.AvatarOrgs>
+                    <h2>Organizations</h2>
+                </S.AvatarOrgs> */}
+                <S.AvatarDetails>
+                    <S.AvatarDetail>
+                        <a href={`http://github.com/${user.company?.substring(1)}`}>
+                            <strong>{user.company}</strong>
+                        </a>
+                    </S.AvatarDetail>
+                    {
+                        user.location ? (
+                            <S.AvatarDetail>
+                                {user.location}
+                            </S.AvatarDetail>
+                        ) : null
+                    }
+                    {
+                        user.email ? (
+                            <S.AvatarDetail>
+                                {user.email}
+                            </S.AvatarDetail>
+                        ) : null
+                    }
+                    <S.AvatarDetail>
+                        <a href={`http://${user.blog}`}>{user.blog}</a>
+                    </S.AvatarDetail>
+                    {
+                        user.twitter_username ? (
+                            <S.AvatarDetail>
+                                <Twitter width={25} />
+                                <a href={`https://twitter.com/${user.twitter_username}`}>
+                                    @{user.twitter_username}
+                                </a>
+                            </S.AvatarDetail>
+                        ) : null
+                    }
+                </S.AvatarDetails>
             </S.AvatarBox>
-
-            {user.name}
         </S.Container>
     )  
 }
--- src/components/ProfileInfo/styles.ts
+++ src/components/ProfileInfo/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-    max-width: 320px;
+    /* max-width: 320px; */
     margin-left: 18px;
 `
 
@@ -19,6 +19,14 @@ export const AvatarBox = styled.div`
 export const Avatar = styled.img`
     width: 16%;
     border-radius: 50%;
+    border: 1px solid var(--border-primary);
+
+    overflow: hidden;
+    line-height: 1;
+    vertical-align: middle;
+
+    flex-shrink: 0;
+    box-shadow: 0 0 0 1px var(--avatar-border);
     @media (min-width: 768px) {
         width: 100%;
         /* margin-top: -34px; */
@@ -29,14 +37,10 @@ export const AvatarInfo = styled.div`
     display: flex;
     flex-direction: column;
     margin-left: 16px;
+    width: 100%;
 
     > strong {
         font-size: 26px;
-    }
-
-    > span {
-        font-size: 20px;
-        color: var(--gray);
     }
 
     > p {
@@ -44,8 +48,153 @@ export const AvatarInfo = styled.div`
         margin-top: 20px;
     }
 
+    > h1 {
+        display: flex;
+        flex-direction: column;
+        line-height: 1;
+        font-weight: 600;
+    }
+
     @media (min-width: 768px) {
         margin-left: 0;
         margin-top: 16px;
+    }
+`
+
+export const AvatarFullname = styled.span`
+    font-size: 26px;
+    line-height: 1.25;
+`
+
+export const AvatarNickname = styled.span`
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 24px;
+    color: var(--gray);
+`
+
+export const AvatarDescription = styled.span`
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 24px;
+
+    margin-top: 16px;
+
+    color: var(--gray);
+`
+
+export const FollowButton = styled.button`
+    display: block;
+    width: 100%;
+    text-align: center;
+
+    color: var(--btn-text);
+    background-color: var(--btn-bg);
+    border-color: var(--btn-border);
+    transition: .2s cubic-bezier(.3,0,.5,1);
+    transition-property: color,background-color,border-color;
+
+    user-select: none;
+    border: 1px solid;
+    border-radius: 6px;
+
+    position: relative;
+    padding: 5px 16px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    white-space: nowrap;
+    cursor: pointer;
+
+    &:hover {
+        background-color: var(--btn-hover-bg);
+        border-color: var(--btn-hover-border);
+        transition-duration: .1s;
+    }
+`
+
+export const AvatarValue = styled.a`
+    white-space: nowrap;
+    text-decoration: none;
+    color: var(--text-secondary);
+    font-size: 18px;
+
+    > strong {
+        font-weight: 600;
+        color: var(--text-primary);
+    }
+
+    &:hover {
+        color: var(--link);
+        > strong {
+            color: var(--link);
+        }
+    }
+`
+
+export const AvatarValues = styled.div`
+    margin:16px 0;
+    display: flex;
+    gap: 10px;
+`
+
+export const AvatarBio = styled.div`
+    overflow: hidden;
+    font-size: 17px;
+    margin-bottom: 16px;
+    word-wrap: break-word;
+`
+
+export const AvatarDetails = styled.ul`
+    list-style: none;
+    padding-left: 0;
+    margin-top: 0;
+    margin-bottom: 0;
+`
+
+export const AvatarDetail = styled.li`
+    padding-left: 22px;
+    font-size: 18px;
+    padding-top: 4px;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+
+    vertical-align: top;
+
+    width: 100%;
+    max-width: 100%;
+
+    svg {
+        margin-right: 6px;
+    }
+
+    > a {
+        white-space: nowrap;
+        text-decoration: none;
+
+        > strong {
+            font-weight: 600;
+        }
+
+        &:hover {
+            text-decoration: underline;
+            color: var(--link);     
+        }
+    }
+`
+
+export const AvatarOrgs = styled.div`
+    width: 100%;
+    border-top: 1px solid var(--border);
+    padding-top: 16px;
+
+    > h2 {
+        font-weight: 600;
+        font-size: 16px;
+        margin-bottom: 8px;
     }
 `
