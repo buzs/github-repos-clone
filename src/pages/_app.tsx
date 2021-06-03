11f128dcb846cdd2aca78a41cec7e6fb86e768fe
Gabriel Henrique <contato@gabrielhenrique.net>
2021-06-03 07:55:21 -0300

    change configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { themes } from '../styles/theme'
 import GlobalStyles from '../styles/GlobalStyles'
+import Layout from '../layouts'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={themes.light}>
         <GlobalStyles />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </>
   )
