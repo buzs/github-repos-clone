5f86dde671a38fd7329105a5c585edb81f100684
Gabriel Henrique <contato@gabrielhenrique.net>
2021-06-03 14:00:00 -0300

    update footer style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,25 @@
 import * as S from './styles'
+import {Github} from '@styled-icons/boxicons-logos/Github'
 
 const Footer: React.FC = () => {
     return (
         <S.Container>
-            © 2021 GitHub, Inc.
+            <S.NavList>
+                © 2021 GitHub, Inc. 
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Security</li>
+                <li>Status</li>
+                <li>Docs</li>
+            </S.NavList>
+            <Github width={45} />
+            <S.NavList>
+                <li>Contact Github</li>
+                <li>Pricing</li>
+                <li>API</li>
+                <li>Training</li>
+                <li>Blog</li>
+            </S.NavList>
         </S.Container>
     )  
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,8 +4,33 @@ export const Container = styled.footer`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 40px 0 8px;
+    border-top: 1px solid var(--border);
+    max-width: 1280px;
+    margin: 40px auto 8px;
+    
+    @media (min-width: 698px) {
+        flex-direction: row;
+        justify-content: space-between;
+    }
 `
 
 export const Link = styled.a`
     color: aliceblue;
+`
+
+export const NavList = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    > li {
+        margin: 0 10px;
+        color: var(--link);
+        font-size: 12px;
+        cursor: pointer;
+        &:hover {
+        text-decoration: underline;
+        }
+    }
 `
