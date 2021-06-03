ccbc6441766909a48878cfb76b3da6caa793f053
Gabriel Henrique <contato@gabrielhenrique.net>
2021-06-03 14:00:57 -0300

    update repo item styles

--- src/components/RepoItem/index.tsx
+++ src/components/RepoItem/index.tsx
@@ -1,10 +1,54 @@
-import { Container } from './styles'
+import { Link } from '../Footer/styles'
+import * as S from './styles'
 
-const RepoItem: React.FC = () => {
+type RepoItemProps = {
+    repo: {
+        name: string,
+        description: string;
+        topics?: string[];
+        stargazers_count: number;
+        html_url: string;
+        license?: {
+            name: string
+        }
+        updated_at: string;
+    }
+}
+
+const RepoItem: React.FC<RepoItemProps> = ({ repo }) => {
     return (
-        <Container>
-            RepoItem
-        </Container>
+        <S.Container>
+            <S.InfoBox>
+                <S.Title>
+                    <a href={repo.html_url}>
+                        {repo.name}
+                    </a>
+                </S.Title>
+                <S.Description>
+                    {repo.description}
+                </S.Description>
+                <S.Topics>
+                    {repo.topics?.map(topic => <S.Topic key={topic}>{topic}</S.Topic>)}
+                </S.Topics>
+                <S.SecondaryBox>
+                    <S.Stars>
+                        {repo.stargazers_count}
+                    </S.Stars>
+
+                    <S.License>
+                        {repo.license?.name}
+                    </S.License>
+
+                    <S.Updated>
+                        {repo.updated_at}
+                    </S.Updated>
+
+                </S.SecondaryBox>
+            </S.InfoBox>
+            <S.LeftBox>
+                <S.Button>Star</S.Button>
+            </S.LeftBox>
+        </S.Container>
     )  
 }
 
--- src/components/RepoItem/styles.ts
+++ src/components/RepoItem/styles.ts
@@ -3,8 +3,77 @@ import styled, { css } from 'styled-components';
 export const Container = styled.li`
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    width: 100%;
 `
 
-export const Title = styled.a`
-    
+export const InfoBox = styled.div`
+
+`
+
+export const LeftBox = styled.div`
+    display: flex;
+    justify-content: row;
+`
+
+export const Title = styled.h3`
+    word-break: break-all;
+    font-weight: 600;
+    font-size: 20px;
+
+    > a {
+        outline-width: 0;
+        color: var(--link);
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`
+
+export const Description = styled.p`
+    color: var(--text-secondary)
+`
+
+export const Topics = styled.div`
+    height: 30px;
+    display: inline-flex;
+`
+
+export const Topic = styled.div``
+
+export const SecondaryBox = styled.div`
+    display: flex;
+`
+
+export const Stars = styled.div``
+
+export const License = styled.div``
+
+export const Updated = styled.div``
+
+export const Button = styled.button`
+    padding: 3px 12px;
+    font-size: 12px;
+    line-height: 20px;
+
+    color: var(--btn-text);
+    background-color: var(--btn-bg);
+    border-color: var(--btn-border);
+    transition: .2s cubic-bezier(.3,0,.5,1);
+    transition-property: color,background-color,border-color;
+
+    user-select: none;
+    border: 1px solid;
+    border-radius: 6px;
+    appearance: none;
+    white-space: nowrap;
+    vertical-align: middle;
+    cursor: pointer;
+
+    &:hover {
+        background-color: var(--btn-bg-hover);
+        border-color: var(--btn-border-hover);
+    }
 `
