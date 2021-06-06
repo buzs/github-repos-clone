ae3e2fbbb9bd3b013ea6c87767e0115c29a1005c
Buz <contato@gabrielhenrique.net>
2021-06-06 00:14:00 -0300

    try remove undefined api call

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -13,25 +13,27 @@ const UserProfile = () => {
   const [data, setData] = useState<any>();
 
   useEffect(() => {
-    Promise.all([
-      fetch(`https://api.github.com/users/${username}`),
-      fetch(`https://api.github.com/users/${username}/repos?sort=pushed`),
-    ]).then(async responses => {
-      const [userResponse, reposResponse] = responses;
-
-      if (userResponse.status === 404) {
-        setData({ error: 'User not found!' })
-        return;
-      }
-
-      const user = await userResponse.json();
-      const repos = await reposResponse.json();
-
-      setData({
-        user,
-        repos
+    if (username) {
+      Promise.all([
+        fetch(`https://api.github.com/users/${username}`),
+        fetch(`https://api.github.com/users/${username}/repos?sort=pushed`),
+      ]).then(async responses => {
+        const [userResponse, reposResponse] = responses;
+  
+        if (userResponse.status === 404) {
+          setData({ error: 'User not found!' })
+          return;
+        }
+  
+        const user = await userResponse.json();
+        const repos = await reposResponse.json();
+  
+        setData({
+          user,
+          repos
+        });
       });
-    });
+    }
   }, [username]);
 
   if (!data?.user || !data?.repos) {
